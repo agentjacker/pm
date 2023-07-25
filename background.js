0e0224a339bf5cc0a574e3a4fdd3f04117335da3
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-07-24 23:27:42 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,97 +1,107 @@
-var tab_listeners = {};
-var tab_push = {}, tab_lasturl = {};
-var selectedId = -1;
+const tab_listeners = {};
+const tab_push = {};
+const tab_lasturl = {};
+let selectedId = -1;
 
 function refreshCount() {
-	txt = tab_listeners[selectedId] ? tab_listeners[selectedId].length : 0;
-	chrome.tabs.get(selectedId, function() {
-		if (!chrome.runtime.lastError) {
-			chrome.browserAction.setBadgeText({"text": ''+txt, tabId: selectedId});
-			if(txt > 0) {
-				chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255]});
-			} else {
-				chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 255, 0] });
-			}
-		}
-	});
+  const txt = tab_listeners[selectedId] ? tab_listeners[selectedId].length : 0;
+  chrome.tabs.get(selectedId, function(tab) {
+    if (chrome.runtime.lastError) {
+      return; // Error occurred while getting the tab info, handle it if needed.
+    }
+
+    chrome.browserAction.setBadgeText({ text: '' + txt, tabId: selectedId });
+    if (txt > 0) {
+      chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+    } else {
+      chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 255, 0] });
+    }
+  });
 }
 
 function logListener(data) {
-	chrome.storage.sync.get({
-		log_url: ''
-	}, function(items) {
-		log_url = items.log_url;
-		if(!log_url.length) return;
-		data = JSON.stringify(data);
-		try {
-			fetch(log_url, {
-				method: 'post',
-				headers: {
-					"Content-type": "application/json; charset=UTF-8"
-				},
-				body: data
-			});
-		} catch(e) { }
-	});
+  chrome.storage.sync.get({
+    log_url: ''
+  }, function(items) {
+    const log_url = items.log_url;
+    if (!log_url.length) return;
+    data = JSON.stringify(data);
+    try {
+      fetch(log_url, {
+        method: 'post',
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        },
+        body: data
+      });
+    } catch (e) {
+      console.error('Error sending data:', e);
+    }
+  });
 }
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-	console.log('message from cs', msg);
-	tabId = sender.tab.id;
-	if(msg.listener) {
-		if(msg.listener == 'function () { [native code] }') return;
-		msg.parent_url = sender.tab.url;
-		if(!tab_listeners[tabId]) tab_listeners[tabId] = [];
-		tab_listeners[tabId][tab_listeners[tabId].length] = msg;
-		logListener(msg);
-	}
-	if(msg.pushState) {
-		tab_push[tabId] = true;
-	}
-	if(msg.changePage) {
-		delete tab_lasturl[tabId];
-	}
-	if(msg.log) {
-		console.log(msg.log);
-	} else {
-		refreshCount();
-	}
+  console.log('message from cs', msg);
+  const tabId = sender.tab.id;
+  if (msg.listener && msg.listener !== 'function () { [native code] }') {
+    msg.parent_url = sender.tab.url;
+    tab_listeners[tabId] = tab_listeners[tabId] || [];
+    tab_listeners[tabId].push(msg);
+    logListener(msg);
+  }
+
+  if (msg.pushState) {
+    tab_push[tabId] = true;
+  }
+
+  if (msg.changePage) {
+    delete tab_lasturl[tabId];
+  }
+
+  if (!msg.log) {
+    refreshCount();
+  }
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, props) {
-	console.log(props);
-	if (props.status == "complete") {
-		if(tabId == selectedId) refreshCount();
-	} else if(props.status) {
-		if(tab_push[tabId]) {
-			//this was a pushState, ignore
-			delete tab_push[tabId];
-		} else {
-			//if(props.url && tab_lasturl[tabId] && props.url.split('#')[0] == tab_lasturl[tabId]) {
-				//same url as before, only a hash change, ignore
-			//} else 
-			if(!tab_lasturl[tabId]) {
-				//wipe on other statuses, but only if lastpage is not set (aka, changePage did not run)
-				tab_listeners[tabId] = [];
-			}
-		}
-	}
-	if(props.status == "loading")
-		tab_lasturl[tabId] = true;
+  console.log(props);
+  if (props.status === "complete") {
+    if (tabId === selectedId) {
+      refreshCount();
+    }
+  } else if (props.status) {
+    if (tab_push[tabId]) {
+      // This was a pushState, ignore
+      delete tab_push[tabId];
+    } else {
+      //if (props.url && tab_lasturl[tabId] && props.url.split('#')[0] == tab_lasturl[tabId]) {
+        // Same URL as before, only a hash change, ignore
+      //} else 
+      if (!tab_lasturl[tabId]) {
+        // Wipe on other statuses, but only if last page is not set (i.e., changePage did not run)
+        tab_listeners[tabId] = [];
+      }
+    }
+  }
+
+  if (props.status === "loading") {
+    tab_lasturl[tabId] = true;
+  }
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-	selectedId = activeInfo.tabId;
-	refreshCount();
+  selectedId = activeInfo.tabId;
+  refreshCount();
 });
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	selectedId = tabs[0].id;
-	refreshCount();
+// Initial setup
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  selectedId = tabs[0].id;
+  refreshCount();
 });
 
 chrome.extension.onConnect.addListener(function(port) {
-	port.onMessage.addListener(function(msg) {
-		port.postMessage({listeners:tab_listeners});
-	});
-})
+  port.onMessage.addListener(function(msg) {
+    port.postMessage({ listeners: tab_listeners });
+  });
+});
