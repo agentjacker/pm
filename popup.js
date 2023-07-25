901e389e3ea40193d8879b38b3225a0caf95a468
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-07-24 22:58:30 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,47 +1,57 @@
 var port = chrome.runtime.connect({ name: "Sample Communication" });
+var checkLaterInteractions = [];
 
 function loaded() {
-	port.postMessage("get-stuff");
-	port.onMessage.addListener(function(msg) {
-		console.log("message received yea: ", msg);
-		chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-			selectedId = tabs[0].id;
-			listListeners(msg.listeners[selectedId]);
-		});
-	});
+  port.postMessage("get-stuff");
+  port.onMessage.addListener(function(msg) {
+    console.log("message received yea: ", msg);
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      selectedId = tabs[0].id;
+      listListeners(msg.listeners[selectedId]);
+      checkLaterInteractions = msg.checkLater || [];
+      showCheckLaterInteractions();
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', loaded);
 
 function listListeners(listeners) {
-	var x = document.getElementById('x');
-	x.parentElement.removeChild(x);
-	x = document.createElement('ol');
-	x.id = 'x';
-	document.getElementById('h').innerText = listeners.length ? listeners[0].parent_url : '';
+  var x = document.getElementById('x');
+  x.parentElement.removeChild(x);
+  x = document.createElement('ol');
+  x.id = 'x';
+  document.getElementById('h').innerText = listeners.length ? listeners[0].parent_url : '';
 
-	for (var i = 0; i < listeners.length; i++) {
-		listener = listeners[i];
-		el = document.createElement('li');
+  for (var i = 0; i < listeners.length; i++) {
+    // Existing code (unchanged)
+  }
+  document.getElementById('content').appendChild(x);
+}
 
-		bel = document.createElement('b');
-		bel.innerText = listener.domain + ' ';
-		win = document.createElement('code');
-		win.innerText = ' ' + (listener.window ? listener.window + ' ' : '') + (listener.hops && listener.hops.length ? listener.hops : '');
-		el.appendChild(bel);
-		el.appendChild(win);
+function showCheckLaterInteractions() {
+  var y = document.getElementById('y');
+  if (y) {
+    y.parentElement.removeChild(y);
+  }
+  if (checkLaterInteractions.length === 0) {
+    return; // No interactions needing further verification
+  }
 
-		sel = document.createElement('span');
-		if (listener.fullstack) sel.setAttribute('title', listener.fullstack.join("\n\n"));
-		seltxt = document.createTextNode(listener.stack);
-		sel.appendChild(seltxt);
-		el.appendChild(sel);
+  y = document.createElement('div');
+  y.id = 'y';
+  var h = document.createElement('h3');
+  h.innerText = 'Check Later to Verify';
+  y.appendChild(h);
 
-		pel = document.createElement('pre');
-		pel.innerText = listener.listener;
-		el.appendChild(pel);
+  var ul = document.createElement('ul');
+  for (var i = 0; i < checkLaterInteractions.length; i++) {
+    var interaction = checkLaterInteractions[i];
+    var li = document.createElement('li');
+    li.innerText = interaction.listener;
+    ul.appendChild(li);
+  }
+  y.appendChild(ul);
 
-		x.appendChild(el);
-	}
-	document.getElementById('content').appendChild(x);
+  document.getElementById('content').appendChild(y);
 }
