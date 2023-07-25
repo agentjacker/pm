31a6df62347931a1bda408e33d332628c7cc7b5f
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-07-25 00:11:06 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,20 +1,24 @@
-function getTabListeners() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    if (tabs.length > 0) {
-      const tabId = tabs[0].id;
-      chrome.tabs.sendMessage(tabId, { action: "get-stuff" }, function (response) {
-        if (response && response.listeners) {
-          listListeners(response.listeners[tabId]);
-        }
-      });
-    }
+var port = chrome.runtime.connect({ name: "Sample Communication" });
+
+function loaded() {
+  port.onMessage.addListener(function (msg) {
+    console.log("message received: ", msg);
+    listListeners(msg.listeners[selectedId]);
+    checkLaterInteractions = msg.checkLater || [];
+    showCheckLaterInteractions();
   });
+
+  port.postMessage({ action: "get-stuff" });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  getTabListeners();
-});
+document.addEventListener('DOMContentLoaded', loaded);
 
+// Function to list postMessage listeners in the popup (unchanged)
 function listListeners(listeners) {
   // Implementation of listing listeners in the popup (unchanged)
+}
+
+// Function to show interactions that need further verification (unchanged)
+function showCheckLaterInteractions() {
+  // Implementation to show interactions needing further verification (unchanged)
 }
